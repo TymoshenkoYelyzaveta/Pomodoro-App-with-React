f1259306d6e5224d8ed66e1d814413f997c8fd08
Liza <tymoshenkoyelyzaveta.s@gmail.com>
2020-09-07 15:24:53 +0200

    pause and reset functionality added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { FaPlayCircle, FaPauseCircle, FaSquare } from "react-icons/fa";
 import "./App.css";
 
@@ -9,16 +9,39 @@ function padTime(time) {
 export default function App() {
   const [title, setTitle] = useState("Start your timer");
   const [timeLeft, setTimeLeft] = useState(25 * 60);
+  const [isRunning, setIsRunning] = useState(false);
+  const intervalRef = useRef(null);
 
   function startTimer() {
-    setInterval(() => {
+    if (intervalRef.current !== null) return;
+
+    setTitle("Keep going !!!");
+    setIsRunning(true);
+    intervalRef.current = setInterval(() => {
       setTimeLeft((timeLeft) => {
         if (timeLeft >= 1) return timeLeft - 1;
+        resetTimer();
         return 0;
       });
     }, 1000);
   }
 
+  function pauseTimer() {
+    if (intervalRef.current === null) return;
+
+    clearInterval(intervalRef.current);
+    intervalRef.current = null;
+    setTitle("You stopped your timer :(");
+    setIsRunning(false);
+  }
+
+  function resetTimer() {
+    clearInterval(intervalRef.current);
+    intervalRef.current = null;
+    setTitle("Another round? :)");
+    setTimeLeft(25 * 60);
+    setIsRunning(false);
+  }
   const minutes = padTime(Math.floor(timeLeft / 60));
 
   const seconds = padTime(timeLeft - minutes * 60);
@@ -33,13 +56,17 @@ export default function App() {
       </div>
 
       <div className="buttons">
-        <button onClick={startTimer}>
-          <FaPlayCircle />
-        </button>
-        <button>
-          <FaPauseCircle />
-        </button>
-        <button>
+        {!isRunning && (
+          <button onClick={startTimer}>
+            <FaPlayCircle />
+          </button>
+        )}
+        {isRunning && (
+          <button onClick={pauseTimer}>
+            <FaPauseCircle />
+          </button>
+        )}
+        <button onClick={resetTimer}>
           <FaSquare />
         </button>
       </div>
